9391f378846378cce8a870a8c7f5d3a7c5377de1
Abega1642 <hei.abega.2@gmail.com>
2025-01-05 20:04:30 +0300

    feat: implement substitution sort

---
+++ substitutionSort.c
@@ -0,0 +1,41 @@
+
+#include <stdio.h>
+
+int findMin(int array[], int size) {
+    int min = array[0];
+    for (int i = 0; i < size; i++) {
+        if (min > array[i]) {
+            min = array[i];
+        }
+    }
+    return min;
+}
+
+int findMax(int array[], int size) {
+    int max = array[0];
+    for (int i = 0; i < size; i++) {
+        if (max < array[i]) {
+            max = array[i];
+        }
+    }
+    return max;
+}
+
+void substitutionSort(int array[], const int arraySize) {
+    const int maximumValue = findMax(array, arraySize);
+    int sortedArray[arraySize];
+    for (int i = 0; i < arraySize; i++) {
+        const int min = findMin(array, arraySize);
+        for (int j = 0; j < arraySize; j++) {
+            if (min == array[j]) {
+                sortedArray[i] = array[j];
+                array[j] = maximumValue;
+            }
+        }
+    }
+    for (int i = 0; i < arraySize; i++) {
+        array[i] = sortedArray[i];
+    }
+}
+
+

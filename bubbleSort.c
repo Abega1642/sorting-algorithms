afd3d9bfe126a76941f21516fbc70e338f4ea9c5
Abega1642 <hei.abega.2@gmail.com>
2025-01-05 20:03:15 +0300

    feat: implement buble sort algorithm

---
+++ bubbleSort.c
@@ -0,0 +1,17 @@
+
+#include <stdio.h>
+
+void bubbleSort(int arr[], int n) {
+    for (int i = 0; i < n; i++) {
+        int max = arr[0];
+        for (int j = 1; j < n; j++) {
+            if (max > arr[j]) {
+                arr[j - 1] = arr[j];
+                arr[j] = max;
+            } else {
+                max = arr[j];
+            }
+        }
+    }
+}
+
